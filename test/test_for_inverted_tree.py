c57de6105ce03852b1ddc49b52453795e2405682
Olha <151624725+OlhaLeno@users.noreply.github.com>
2024-03-25 11:54:52 +0200

    Create test_for_inverted_tree.py

---
+++ test/test_for_inverted_tree.py
@@ -0,0 +1,19 @@
+import unittest
+from lab3v3l2 import invert_binary_tree, BinaryTree
+
+
+class InvertationCase(unittest.TestCase):
+    def test_1(self):
+        tree = BinaryTree(1)
+        tree.left = BinaryTree(2)
+        tree.right = BinaryTree(3)
+
+        inverted_tree = invert_binary_tree(tree)
+
+        self.assertEqual(inverted_tree.value, 1)
+        self.assertEqual(inverted_tree.left.value, 3)
+        self.assertEqual(inverted_tree.right.value, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
