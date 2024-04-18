5133c448385402228fc2b040790095b1f7632e75
Olha <151624725+OlhaLeno@users.noreply.github.com>
2024-04-19 01:51:33 +0300

    Create gas_supply_between_cities.py

---
+++ src/gas_supply_between_cities.py
@@ -0,0 +1,34 @@
+def dfs(graph, start, visited):
+    # if visited is None:
+    #     visited = set()
+    visited.add(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+    return
+
+
+def gas_supply(cities, storages, pipes):
+    graph = {}
+    for city in cities + storages:
+        graph[city] = []
+
+    for pipe in pipes:
+        source, end = pipe
+        graph[source].append(end)
+
+    unreachable_cities = {}
+    for storage in storages:
+        visited = set()
+        dfs(graph, storage, visited)
+        unreachable = set(cities) - visited
+        if unreachable:
+            unreachable_cities[storage] = list(unreachable)
+
+    return unreachable_cities
+
+
+cities = ['Lviv', 'Striy', 'Dolina', 'Kyiv', 'Mykolaiv']
+storages = ['storage_1', 'storage_2']
+pipes = [['Lviv', 'Striy'], ['storage_1', 'Lviv'], ['Lviv', 'Dolina'], ['Lviv', 'Kyiv']]
+print(gas_supply(cities, storages, pipes))
