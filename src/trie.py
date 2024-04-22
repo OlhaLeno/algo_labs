1b288470c3b0db7dba3038236df2a2d140e6e317
Olha <151624725+OlhaLeno@users.noreply.github.com>
2024-04-22 23:26:01 +0300

    Create trie.py

---
+++ src/trie.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for letter in word:
+            if letter not in current.children:
+                current.children[letter] = TrieNode()
+            current = current.children[letter]
+        current.endOfWord = True
+
+    def find(self, word: str) -> bool:
+        current = self.root
+        for letter in word:
+            if letter not in current.children:
+                return False
+            current = current.children[letter]
+        return current.endOfWord
+
+    def find_prefix(self, prefix: str) -> bool:
+        current = self.root
+        for letter in prefix:
+            if letter not in current.children:
+                return False
+            current = current.children[letter]
+        return True
