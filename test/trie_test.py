4f69416c00ea8074f4c21ac1b3ead63fc715af85
Olha <151624725+OlhaLeno@users.noreply.github.com>
2024-04-22 23:27:37 +0300

    Create trie_test.py

---
+++ test/trie_test.py
@@ -0,0 +1,26 @@
+import unittest
+from trie import Trie
+
+
+class TrieTest(unittest.TestCase):
+    def setUp(self):
+        self.trie = Trie()
+
+    def test_find_prefix(self):
+        self.trie.insert('banana')
+        self.trie.insert('baobab')
+        self.assertEqual(self.trie.find_prefix('ba'), True)
+        self.assertEqual(self.trie.find_prefix('ba'), True)
+
+    def test_insert_and_find_prefix(self):
+        self.trie.insert('banana')
+        self.trie.find_prefix('ba')
+        self.assertEqual(self.trie.find_prefix('na'), False)
+
+    def test_nothing(self):
+        self.trie.insert('')
+        self.assertEqual(self.trie.insert(''), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
